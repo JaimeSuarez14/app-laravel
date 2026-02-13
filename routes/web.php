a336649362592268e19a0eacaabb394cd8fff60e
Jaime Suarez <jaisua8@gmail.com>
2026-02-13 16:28:38 -0500

    creacion de vistar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcomes');
 });
+
+Route::get('/test', function() {
+    return view('test');
+});
+
+Route::get('/crud', function() {
+    return view('crud.index');
+});
