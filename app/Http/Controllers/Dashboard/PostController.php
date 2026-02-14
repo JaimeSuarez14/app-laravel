9ae9d903459a9316c83cd450fc41fba12255debe
Jaime Suarez <jaisua8@gmail.com>
2026-02-14 18:45:50 -0500

    creacion del tipadp de return

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         $posts = Post::all();
         return  view("post.index", compact('posts'));
