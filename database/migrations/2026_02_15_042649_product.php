c279f76af6d5693e21aed4e2d91606d44c541089
Jaime Suarez <jaisua8@gmail.com>
2026-02-15 20:00:18 -0500

    creacion de la migracion product

--- database/migrations/2026_02_15_042649_product.php
+++ database/migrations/2026_02_15_042649_product.php
@@ -11,7 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('price',8, 2);
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**
